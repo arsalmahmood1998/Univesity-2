ec76c29ce2d3661bd7c53a903d6f8caef8b7ec17
arsalmahmood1998 <68080848+arsalmahmood1998@users.noreply.github.com>
2020-09-22 14:58:04 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,14 @@ public class Main {
 
 	public static void main(String[] args)  {
 		try {
-		
-		System.out.println("Database Connected");
-		App myApp=new App();
-		myApp.run(connect);
-		}
-		
+			
+			System.out.println("Database Connected");
+			App myApp=new App();
+			myApp.run(connect);
+			}
 		catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Database Not Connected");
+			}
 		}
 	}
-	
-}
