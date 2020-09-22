abd9f25520937a9818a834ab34d5b894dbe37d4a
arsalmahmood1998 <68080848+arsalmahmood1998@users.noreply.github.com>
2020-09-22 14:25:47 +0500

    Update Staff.java

--- src/People/Staff.java
+++ src/People/Staff.java
@@ -1,5 +1,7 @@
 package People;
 
+import Divisions.Course;
+
 //import java.util.ArrayList;
 
 import Divisions.Department;
@@ -8,6 +10,7 @@ public class Staff extends Person {
 	private int id;
 	private Staff supervisor;
 	private Department department;
+	private Course course;
 	
 	public Staff(String name, String phone, String email, int id, Staff supervisor) {
 		super(name, phone, email);
@@ -37,23 +40,33 @@ public class Staff extends Person {
 	public void setSupervisor(Staff supervisor) {
 		this.supervisor = supervisor;
 	}
-	public Department getDepartments() {
+
+	public Department getDepartment() {
 		return department;
 	}
 
-	public void setDepartments(Department department) {
+	public void setDepartment(Department department) {
 		this.department = department;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
 	}
 
 	@Override
 	public String toString() {
 		return "Staff [id=" + id + ", " + (supervisor != null ? "supervisor=" + supervisor + ", " : "")
 				+ (department != null ? "department=" + department + ", " : "")
+				+ (course != null ? "course=" + course + ", " : "")
 				+ (getName() != null ? "getName()=" + getName() + ", " : "")
 				+ (getPhone() != null ? "getPhone()=" + getPhone() + ", " : "")
 				+ (getEmail() != null ? "getEmail()=" + getEmail() + ", " : "")
 				+ (super.toString() != null ? "toString()=" + super.toString() + ", " : "")
 				+ (getClass() != null ? "getClass()=" + getClass() + ", " : "") + "hashCode()=" + hashCode() + "]";
 	}
-
+	
 }
